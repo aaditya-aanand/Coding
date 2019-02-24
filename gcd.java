c8c958122b20964bb231263eb9ab79885b435379
Aaditya Aanand <aaaditya.nnnn@gmail.com>
2019-02-24 21:36:40 +0530

    Euclid's Algorithm

---
+++ gcd.java
@@ -0,0 +1,19 @@
+public static long gcd(long a, long b)
+{
+    return b == 0 ? a : gcd(b, a%b);
+}
+
+public static long gcd(long a, long b)
+{
+    long dividend = a >= b ? a : b;
+    long divisor = a <= b ? a : b;
+
+    while(divisor != 0)
+    {
+        long remainder = dividend%divisor;
+        dividend = divisor;
+        divisor = remainder;
+    }
+
+    return dividend;
+}
