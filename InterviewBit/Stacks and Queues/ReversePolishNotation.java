fa18e3d4264586028aab59319095e82600a42642
Aaditya Aanand <aaaditya.nnnn@gmail.com>
2019-02-26 22:30:15 +0530

    Value of an Arithmetic Expression given in Reverse Polish Notation

---
+++ InterviewBit/Stacks and Queues/ReversePolishNotation.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int evalRPN(String[] A) 
+    {
+        Stack<Integer> stack = new Stack<Integer>();
+        HashSet<Character> set = new HashSet<Character>();
+        set.add('-'); set.add('+'); set.add('*'); set.add('/');
+        
+        int len = A.length;
+        for(int i = 0; i<len; i++)
+        {
+            char x = A[i].charAt(0);
+            if(A[i].length() == 1 && set.contains(x))
+            {
+                if(x == '-')
+                stack.push(-1*stack.pop()+stack.pop());
+                
+                else if(x == '+')
+                stack.push(stack.pop()+stack.pop());
+                
+                else if(x == '*')
+                stack.push(stack.pop()*stack.pop());
+                
+                else
+                {
+                    int temp = stack.pop();
+                    stack.push(stack.pop()/temp);
+                }
+            }
+
+            else
+                stack.push(Integer.valueOf(A[i]));
+        }
+        
+        return stack.pop();
+    }
+}
