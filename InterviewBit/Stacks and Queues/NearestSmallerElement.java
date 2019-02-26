de289226fe91f891e67a0748a73a3365c38e2610
Aaditya Aanand <aaaditya.nnnn@gmail.com>
2019-02-26 23:31:30 +0530

    Nearest smallest number to the left of every element

--- InterviewBit/Stacks and Queues/NearestSmallerElement.java
+++ InterviewBit/Stacks and Queues/NearestSmallerElement.java
@@ -1,3 +1,9 @@
+/*
+Given an array, find the nearest smaller element G[i] for every element A[i] in the array,
+such that the element has an index smaller than i. Elements for which no smaller element exist, consider next smaller element as -1.
+*/
+
+
 public class Solution {
     public int[] prevSmaller(int[] A) {
         int len = A.length;
