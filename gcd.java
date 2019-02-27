ce6f5e704dc4f31b19bd56b4f8ae36f8b057cea4
Aaditya Aanand <aaaditya.nnnn@gmail.com>
2019-02-28 00:13:57 +0530

    Update gcd.java

--- gcd.java
+++ gcd.java
@@ -1,8 +1,10 @@
+/* Recursive approach */
 public static long gcd(long a, long b)
 {
     return b == 0 ? a : gcd(b, a%b);
 }
 
+/* Iterative approach */
 public static long gcd(long a, long b)
 {
     long dividend = a >= b ? a : b;
