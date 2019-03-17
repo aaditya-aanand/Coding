61aad264d87b5172c15e26586d73fd66c0fd410a
Aaditya Aanand <aaaditya.nnnn@gmail.com>
2019-03-17 13:33:34 +0530

    Create BalancedBinaryTree.java

---
+++ InterviewBit/Tree/BalancedBinaryTree.java
@@ -0,0 +1,63 @@
+/*
+Given a binary tree, determine if it is height-balanced.
+
+ Height-balanced binary tree : is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1. 
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+
+Example :
+
+Input : 
+          1
+         / \
+        2   3
+
+Return : True or 1 
+
+Input 2 : 
+         3
+        /
+       2
+      /
+     1
+
+Return : False or 0 
+         Because for the root node, left subtree has depth 2 and right subtree has depth 0. 
+         Difference = 2 > 1.
+*/
+
+public class Solution 
+{
+    public int isBalanced(TreeNode A) 
+    {
+        if(A == null)
+            return 1;
+
+        if(A.left == null && A.right != null)
+        {
+            if(A.right.left != null || A.right.right != null)
+                return 0;
+        }
+
+        if(A.left != null && A.right == null)
+        {
+            if(A.left.left != null || A.left.right != null)
+                return 0;
+        }
+
+        return isBalanced(A.left)*isBalanced(A.right);
+    }
+}
+
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
