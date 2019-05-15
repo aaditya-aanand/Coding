0c5e8f66b7eaa650a7396432b0c3eff953dea73d
Aaditya Aanand <aaaditya.nnnn@gmail.com>
2019-05-15 08:38:26 +0530

    Updated a new method

--- InterviewBit/Dynamic Programming/MaxRectangleInBinaryMatrix.java
+++ InterviewBit/Dynamic Programming/MaxRectangleInBinaryMatrix.java
@@ -55,3 +55,61 @@ public class Solution
         return max;
     }
 }
+
+//Laying histogram method by STACK
+
+public class Solution
+{
+    public int maximalRectangle(int[][] A)
+    {
+        int rows = A.length;
+        int cols = A[0].length;
+
+        for(int i = 0; i<rows; i++)
+            for(int j = cols-2; j>=0; j--)
+                A[i][j] = (A[i][j+1] == 0) ? A[i][j] : A[i][j]*(A[i][j] + A[i][j+1]);
+        
+        int maximum = 0;
+        for(int j = 0; j<cols; j++)
+        {
+            int next_small_left_index[] = new int[rows];
+            int next_small_right_index[] = new int[rows];
+
+            Stack<Integer> left = new Stack<Integer>();
+            Stack<Integer> right = new Stack<Integer>();
+
+            for(int i = 0; i<rows; i++)
+            {
+                int x = A[i][j];
+                while(!left.isEmpty() && A[left.peek()][j] >= x)
+                    left.pop();
+
+                next_small_left_index[i] = (left.isEmpty()) ? -1 : left.peek();
+                left.push(i);
+            }
+
+            for(int i = rows-1; i>=0; i--)
+            {
+                int x = A[i][j];
+                while(!right.isEmpty() && A[right.peek()][j] >= x)
+                    right.pop();
+
+                next_small_right_index[i] = (right.isEmpty()) ? rows : right.peek();
+                right.push(i);
+            }
+
+            int max = 0;
+            for(int i = 0; i<rows; i++)
+            {
+                int l = next_small_left_index[i];
+                int r = next_small_right_index[i];
+
+                max = Math.max(max, (r-l-1)*A[i][j]);
+            }
+
+            maximum = Math.max(maximum, max);
+        }
+
+        return maximum;
+    }
+}
